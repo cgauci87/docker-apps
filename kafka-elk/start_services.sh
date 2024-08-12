fb8e1b33eaa44090916f23be4c970f49a85bfacb
cgauci87 <cgauci@gmail.com>
2024-08-12 19:18:23 +1000

    adding a Bash script to  automate the sequential startup of the services -- start_services.sh

---
+++ kafka-elk/start_services.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Start Zookeeper first
+echo "Starting Zookeeper..."
+docker-compose up -d zookeeper
+sleep 30
+
+# Start Kafka after Zookeeper
+echo "Starting Kafka..."
+docker-compose up -d kafka
+sleep 30
+
+# Start Elasticsearch after Kafka
+echo "Starting Elasticsearch..."
+docker-compose up -d elasticsearch
+sleep 30
+
+# Start Logstash after Elasticsearch
+echo "Starting Logstash..."
+docker-compose up -d logstash
+sleep 30
+
+# Start Kibana after Logstash
+echo "Starting Kibana..."
+docker-compose up -d kibana
+sleep 30
+
+# Start Cassandra nodes last
+echo "Starting Cassandra nodes..."
+docker-compose up -d cassandra-dc1 cassandra-dc2
+
+echo "All services started!"
