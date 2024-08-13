cf2d7d561d0faa5151998763035d3706b08c3352
cgauci87 <cgauci@gmail.com>
2024-08-13 21:20:33 +1000

    updating start services bash file to include build

--- kafka-elk/start_services.sh
+++ kafka-elk/start_services.sh
@@ -2,31 +2,31 @@
 
 # Start Zookeeper first
 echo "Starting Zookeeper..."
-docker-compose up -d zookeeper
+docker-compose up --build -d zookeeper
 sleep 30
 
 # Start Kafka after Zookeeper
 echo "Starting Kafka..."
-docker-compose up -d kafka
+docker-compose up --build -d kafka
 sleep 30
 
 # Start Elasticsearch after Kafka
 echo "Starting Elasticsearch..."
-docker-compose up -d elasticsearch
+docker-compose up --build -d elasticsearch
 sleep 30
 
 # Start Logstash after Elasticsearch
 echo "Starting Logstash..."
-docker-compose up -d logstash
+docker-compose up --build -d logstash
 sleep 30
 
 # Start Kibana after Logstash
 echo "Starting Kibana..."
-docker-compose up -d kibana
+docker-compose up --build -d kibana
 sleep 30
 
 # Start Cassandra node last
 echo "Starting Cassandra node..."
-docker-compose up -d cassandra
+docker-compose up --build -d cassandra
 
 echo "All services started!"
